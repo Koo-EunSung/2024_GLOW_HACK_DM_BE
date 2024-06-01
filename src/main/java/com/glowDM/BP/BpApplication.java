401869920a588cf47a7e02ce4219d842e59a7491
Koo-EunSung <ku903@naver.com>
2024-06-01 19:23:10 +0900

    Project Initialize

---
+++ src/main/java/com/glowDM/BP/BpApplication.java
@@ -0,0 +1,13 @@
+package com.glowDM.BP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpApplication.class, args);
+	}
+
+}
